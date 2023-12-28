338179d8ad77bc4868206a09dccbd021c4000cc9
saman <samanghapanuri@gmail.com>
2023-12-28 18:56:41 +0330

    The project was connected to mySQL Data Base

--- config/settings.py
+++ config/settings.py
@@ -88,8 +88,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'swimsmart',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1379S@man',
     }
 }
 
