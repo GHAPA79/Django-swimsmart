d89f139f76a62cb7105af5260e666bee4e8bb489
saman <samanghapanuri@gmail.com>
2024-01-10 17:59:44 +0330

    Create type swim html files for each method

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
+    path('usa-method/', views.USAMethod.as_view(), name='usa-method'),
+    path('aus-method/', views.AUSMethod.as_view(), name='aus-method'),
+    path('uk-method/', views.UKMethod.as_view(), name='uk-method'),
+    path('chn-method/', views.CHNMethod.as_view(), name='chn-method'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,19 @@ from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
     template_name = 'home.html'
+
+
+class USAMethod(TemplateView):
+    template_name = 'usa.html'
+
+
+class AUSMethod(TemplateView):
+    template_name = 'aus.html'
+
+
+class UKMethod(TemplateView):
+    template_name = 'uk.html'
+
+
+class CHNMethod(TemplateView):
+    template_name = 'chn.html'
