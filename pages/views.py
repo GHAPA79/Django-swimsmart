08ed0587721299e86d62e4d8c5b787eeb7a15291
saman <samanghapanuri@gmail.com>
2024-01-13 18:18:52 +0330

    season pages were created

--- pages/views.py
+++ pages/views.py
@@ -21,3 +21,51 @@ class UKMethod(TemplateView):
 
 class CHNMethod(TemplateView):
     template_name = 'chn.html'
+
+
+class USAMethodFast(TemplateView):
+    template_name = 'usa-fast.html'
+
+
+class USAMethodSemiEndu(TemplateView):
+    template_name = 'usa-semi-en.html'
+
+
+class USAMethodEndu(TemplateView):
+    template_name = 'usa-endu.html'
+
+
+class AUSMethodFast(TemplateView):
+    template_name = 'aus-fast.html'
+
+
+class AUSMethodSemiEndu(TemplateView):
+    template_name = 'aus-semi-en.html'
+
+
+class AUSMethodEndu(TemplateView):
+    template_name = 'aus-endu.html'
+
+
+class UKMethodFast(TemplateView):
+    template_name = 'uk-fast.html'
+
+
+class UKMethodSemiEndu(TemplateView):
+    template_name = 'uk-semi-en.html'
+
+
+class UKMethodEndu(TemplateView):
+    template_name = 'uk-endu.html'
+
+
+class CHNMethodFast(TemplateView):
+    template_name = 'chn-fast.html'
+
+
+class CHNMethodSemiEndu(TemplateView):
+    template_name = 'chn-semi-en.html'
+
+
+class CHNMethodEndu(TemplateView):
+    template_name = 'chn-endu.html'
