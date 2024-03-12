576fe1f7fc879d57245062c5f6bf422d432a604f
saman <samanghapanuri@gmail.com>
2024-03-12 15:46:07 +0330

    implement payment proccess

--- orders/models.py
+++ orders/models.py
@@ -11,6 +11,8 @@ class Order(models.Model):
     email = models.EmailField(blank=True, unique=True)
     phone_number = models.CharField(max_length=15)
 
+    zarinpal_authority = models.CharField(max_length=255, blank=True)
+
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,9 @@
 import requests
 import json
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.conf import settings
+from django.http import HttpResponse
 
 from orders.models import Order
 
@@ -23,7 +24,7 @@ def payment_process(request):
         'merchant_id': settings.ZARINPAL_MERCHANT_ID,
         'amount': rial_total_price,
         'description': f'#{order.id}: {order.first_name} {order.last_name}',
-        'callback_url': '127.0.0.1:8000',
+        'callback_url': 'http://127.0.0.1:8000',
     }
 
     request_header = {
@@ -32,3 +33,13 @@ def payment_process(request):
     }
 
     response = requests.post(url=zarinpal_request_url, data=json.dumps(request_data), headers=request_header)
+
+    data = response.json()['data']
+    authority = data['authority']
+    order.zarinpal_authority = authority
+    order.save()
+
+    if 'errors' not in data or len(data['errors']) == 0:
+        return redirect(f'https://www.zarinpal.com/pg/StartPay/{authority}')
+    else:
+        return HttpResponse('Error from ZarinPal')
