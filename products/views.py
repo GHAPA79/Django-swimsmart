60e0c10463482cc367b826261d0030e39a619445
saman <samanghapanuri@gmail.com>
2024-03-25 00:16:57 +0330

    the category ids were modified

--- products/views.py
+++ products/views.py
@@ -7,7 +7,6 @@ from orders.models import OrderItem
 from cart.forms import AddToCartProductForm
 
 
-
 class USAMethodFast(generic.ListView):
     queryset = Product.objects.filter(category_id=1)
     template_name = 'usa-fast.html'
@@ -20,7 +19,7 @@ class USAMethodFast(generic.ListView):
 
 
 class USAMethodSemiEndu(generic.ListView):
-    queryset = Product.objects.filter(category_id=5)
+    queryset = Product.objects.filter(category_id=2)
     template_name = 'usa-semi-en.html'
     context_object_name = 'products'
 
@@ -31,7 +30,7 @@ class USAMethodSemiEndu(generic.ListView):
 
 
 class USAMethodEndu(generic.ListView):
-    queryset = Product.objects.filter(category_id=9)
+    queryset = Product.objects.filter(category_id=3)
     template_name = 'usa-endu.html'
     context_object_name = 'products'
 
@@ -41,41 +40,8 @@ class USAMethodEndu(generic.ListView):
         return context
 
 
-class AUSMethodFast(generic.ListView):
-    queryset = Product.objects.filter(category_id=3)
-    template_name = 'aus-fast.html'
-    context_object_name = 'products'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['add_to_cart_form'] = AddToCartProductForm
-        return context
-
-
-class AUSMethodSemiEndu(generic.ListView):
-    queryset = Product.objects.filter(category_id=7)
-    template_name = 'aus-semi-en.html'
-    context_object_name = 'products'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['add_to_cart_form'] = AddToCartProductForm
-        return context
-
-
-class AUSMethodEndu(generic.ListView):
-    queryset = Product.objects.filter(category_id=11)
-    template_name = 'aus-endu.html'
-    context_object_name = 'products'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['add_to_cart_form'] = AddToCartProductForm
-        return context
-
-
 class UKMethodFast(generic.ListView):
-    queryset = Product.objects.filter(category_id=2)
+    queryset = Product.objects.filter(category_id=4)
     template_name = 'uk-fast.html'
     context_object_name = 'products'
 
@@ -86,7 +52,7 @@ class UKMethodFast(generic.ListView):
 
 
 class UKMethodSemiEndu(generic.ListView):
-    queryset = Product.objects.filter(category_id=6)
+    queryset = Product.objects.filter(category_id=5)
     template_name = 'uk-semi-en.html'
     context_object_name = 'products'
 
@@ -97,7 +63,7 @@ class UKMethodSemiEndu(generic.ListView):
 
 
 class UKMethodEndu(generic.ListView):
-    queryset = Product.objects.filter(category_id=10)
+    queryset = Product.objects.filter(category_id=6)
     template_name = 'uk-endu.html'
     context_object_name = 'products'
 
@@ -107,8 +73,41 @@ class UKMethodEndu(generic.ListView):
         return context
 
 
+class AUSMethodFast(generic.ListView):
+    queryset = Product.objects.filter(category_id=7)
+    template_name = 'aus-fast.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['add_to_cart_form'] = AddToCartProductForm
+        return context
+
+
+class AUSMethodSemiEndu(generic.ListView):
+    queryset = Product.objects.filter(category_id=8)
+    template_name = 'aus-semi-en.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['add_to_cart_form'] = AddToCartProductForm
+        return context
+
+
+class AUSMethodEndu(generic.ListView):
+    queryset = Product.objects.filter(category_id=9)
+    template_name = 'aus-endu.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['add_to_cart_form'] = AddToCartProductForm
+        return context
+
+
 class CHNMethodFast(generic.ListView):
-    queryset = Product.objects.filter(category_id=4)
+    queryset = Product.objects.filter(category_id=10)
     template_name = 'chn-fast.html'
     context_object_name = 'products'
 
@@ -119,7 +118,7 @@ class CHNMethodFast(generic.ListView):
 
 
 class CHNMethodSemiEndu(generic.ListView):
-    queryset = Product.objects.filter(category_id=8)
+    queryset = Product.objects.filter(category_id=11)
     template_name = 'chn-semi-en.html'
     context_object_name = 'products'
 
