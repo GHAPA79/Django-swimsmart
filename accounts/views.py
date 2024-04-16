8251acd240291ad58b0b8a600042d019390e4335
saman <samanghapanuri@gmail.com>
2024-04-17 01:47:28 +0330

    add messages to sign up and profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,24 @@
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .forms import CustomUserCreationForm, ProfileForm
 from .models import CustomUser
 
 
-class SignUpView(generic.CreateView):
+class SignUpView(SuccessMessageMixin, generic.CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+    success_message = 'حساب کاربری شما با موفقیت ساخته شد. لطفا وارد شوید'
 
 
-class Profile(generic.UpdateView):
+class Profile(SuccessMessageMixin, generic.UpdateView):
     model = CustomUser
     template_name = 'registration/profile.html'
     form_class = ProfileForm
     success_url = reverse_lazy('account:profile')
+    success_message = 'حساب کاربری با موفقیت بروزرسانی شد'
 
     def get_object(self, queryset=None):
         return CustomUser.objects.get(pk=self.request.user.pk)
